f9da3eafa4650ce0e8b54881506b7a71ec3868a9
Lize Wu <retniw72@gmail.com>
2020-06-24 14:25:01 +0800

    2020-0624 修改 include_functionbottom_public 符合無障礙2A

--- include_functionbottom_public.php
+++ include_functionbottom_public.php
@@ -1,11 +1,18 @@
 <div class="Function FunctionBottom">
 	<!-- Public -->
-	<a href="#" class="Twitter-BTN"><i class="icon-twitter"></i></a>
+	<!-- <a href="#" class="Twitter-BTN"><i class="icon-twitter"></i></a>
 	<a href="#" class="FB-BTN"><i class="icon-facebook-1"></i></a>
 	<a href="#" class="Plurk-BTN"><i class="icon-plurk"></i></a>
 	<a href="#" class="Google-BTN"><i class="icon-gplus"></i></a>
 	<a href="#" class="line-BTN"><i class="icon-line"></i></a>
-	<a href="#" class="youtube-BTN"><i class="icon-youtube-play"></i></a>
+	<a href="#" class="youtube-BTN"><i class="icon-youtube-play"></i></a> -->
+
+	<a href="#" class="Twitter-BTN">Twitter</a>
+	<a href="#" class="FB-BTN">Facebook</a>
+	<a href="#" class="Plurk-BTN">Plurk</a>
+	<a href="#" class="Google-BTN">Google plus</a>
+	<a href="#" class="line-BTN">line</a>
+	<a href="#" class="youtube-BTN">youtube</a>
 	<!-- Print -->
 	<a href="#" class="icon-print">友善列印</a>
 	<!-- Email -->
--- include_functiontop.php
+++ include_functiontop.php
@@ -1,7 +1,7 @@
 <div class="Function FunctionTop">	
     <!-- Print  -->
     <a href="#" class="icon-print">友善列印</a>
-     Email 
+    <!-- Email  -->
     <a href="#" class="icon-mail-alt">轉寄好友</a>
     <!-- Back -->
     <a href="#" class="icon-reply-1">回上一頁</a>
